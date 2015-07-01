04744e76f0b2d4ff77828e899c31824277908b0f
Søren Færløv <sfr@ordbogen.com>
2015-07-01 09:39:05 +0200

    Fix toggling image toolbar and resize overlay.
    When clicking an image the toolbar and resize overlay appears, clicking it again breaks it until the next edit operation.
    This patch fixes that by forcing an update.

--- src/main.js
+++ src/main.js
@@ -295,6 +295,7 @@ textAngular.directive("textAngular", [
 				scope.hideResizeOverlay = function(){
 					scope.displayElements.resize.anchors[3].off('mousedown', _resizeMouseDown);
 					scope.displayElements.resize.overlay.css('display', '');
+					scope.updateTaBindtaTextElement();
 				};
 
 				// allow for insertion of custom directives on the textarea and div
